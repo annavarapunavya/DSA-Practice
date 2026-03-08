ad019e249da755973eaa6883e7c3c81ee79ce116
annavarapunavya <151749501+annavarapunavya@users.noreply.github.com>
2026-03-08 14:52:31 +0530

    Added LeetCode Two Sum Solution

---
+++ LeetCode/Arrays/Two Sum.py
@@ -0,0 +1,19 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Link: https://leetcode.com/problems/two-sum/
+*/
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in range(len(nums)):
+            if target-nums[i] in d:
+                return [d[target - nums[i]],i]
+            else:
+                d[nums[i]]  = i
