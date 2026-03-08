778043e92056804aea163a100f57c04003b0043f
annavarapunavya <151749501+annavarapunavya@users.noreply.github.com>
2026-03-08 15:08:23 +0530

    Create Single Number.py

---
+++ LeetCode/Bit Manuplation/Single Number.py
@@ -0,0 +1,12 @@
+/*
+Problem: Single Number
+Platform: LeetCode
+Link: https:https://leetcode.com/problems/single-number/
+*/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        num = 0
+        for i in nums:
+            num = num ^ i
+        return num
+        
